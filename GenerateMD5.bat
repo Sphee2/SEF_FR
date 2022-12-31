0b6fbd744bd40538c046476f3b3df615870e48c1
beppegoodoldrebel <stefano.moretti1986@libero.it>
2022-12-31 15:01:51 +0100

    Update GenerateMD5.bat
    dont think it will change anything

--- GenerateMD5.bat
+++ GenerateMD5.bat
@@ -3,7 +3,7 @@
 echo Producing Packages.md5
 
 cd .\System
-..\..\ContentExpansion\System\ucc.exe mastermd5 -c *.u -c SEF/Content/*.ukx -c SEF/Content/Maps/*.s4m -c SEF/Content/*.utx
+..\..\ContentExpansion\System\ucc.exe mastermd5 -c *.u -c SEF_FR/Content/*.ukx -c SEF_FR/Content/Maps/*.s4m -c SEF_FR/Content/*.utx
 
 echo Produced entries:
 
